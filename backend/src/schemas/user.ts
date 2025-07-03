11884d05b7e7927c022dfcefdd66c7775b984fb9
Garrett Lander <garrett.jlander@gmail.com>
2025-07-03 15:08:28 +0200

    first commit

---
+++ backend/src/schemas/user.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod/v4';
+
+const dbEntrySchema = z.strictObject({
+  _id: z.string(),
+  createdAt: z.string()
+});
+
+const userSchema = z.strictObject({
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  email: z.email('Invalid email.'),
+  image: z
+    .url({
+      protocol: /^https?$/,
+      hostname: z.regexes.domain
+    })
+    .default(
+      'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg'
+    )
+});
+
+const userDbSchema = z.strictObject({
+  ...dbEntrySchema.shape,
+  ...userSchema.shape
+});
+
+export { userSchema, userDbSchema };
